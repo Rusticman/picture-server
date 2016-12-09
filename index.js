458310d489f1a70c7e9b337bac4d392e4eb134df
rusticman <russellwheatley85@gmail.com>
2016-12-09 11:26:11 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const app = express();
+const router = require('./router');
+const cors = require('cors');
+
+const config = require('./config');
+
+app.use(morgan('combined'));
+app.use(cors());
+app.use(bodyParser.json({type: "*/*" }));
+
+
+mongoose.connect(config.mongodb_url);
+
+
+router(app);
+
+  const port = process.env.PORT || 3000;
+  const server = http.createServer(app);
+  server.listen(port);
+  console.log('server listening on',port)
